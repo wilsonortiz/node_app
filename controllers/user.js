486e9cba4c46e548cbac9cc18e8653331d4ad599
Wilson Ortiz Troncoso <w.ortizgeek@gmail.com>
2018-04-08 20:30:53 -0300

    Controlador de usuario y autentificación por token

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,10 @@
 'use strict'
 //variables
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt')
-
-
-function pruebas(req, res) {
-	res.status(200).send({
-		message: 'Probado una accion del controlador de usuarios del aPi rest con node y mongo'
-	});
-}
 
 function saveUser(req, res) {
 	var user = new User();
@@ -100,8 +95,99 @@ function loginUser(req, res) {
 	});
 }
 
+function updateUser(req, res) {
+	var userID = req.params.id;
+	var update = req.body;
+
+	User.findByIdAndUpdate(userID, update, (err, userUpdated) => {
+		if (err) {
+			res.status(500).send({
+				message: 'Error al actualizar al usuario.'
+			});
+
+		} else if (!userUpdated) {
+			res.status(404).send({
+				message: 'No se ha podido actualizar el usuario.'
+			});
+
+		} else {
+			res.status(200).send({
+				user: userUpdated
+			});
+		}
+	});
+}
+
+function uploadImage(req, res) {
+	var userID = req.params.id;
+	var file_name = 'No subido...'
+
+	if (req.files) {
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if (file_ext == 'png' || file_ext == 'jpg' ||
+			file_ext == 'gif') {
+
+			User.findByIdAndUpdate(userID, {
+				image: file_name
+			}, (err, userUpdated) => {
+
+				if (err) {
+					res.status(500).send({
+						message: 'Error al actualizar al usuario.'
+					});
+
+				} else if (!userUpdated) {
+					res.status(404).send({
+						message: 'No se ha podido actualizar el usuario.'
+					});
+
+				} else {
+					res.status(200).send({
+						user: userUpdated
+					});
+				}
+			});
+
+		} else {
+			res.status(200).send({
+				message: 'Extensión del archivo no valida.'
+			});
+		}
+
+		console.log(ext_split);
+
+	} else {
+		res.status(200).send({
+			message: 'No ha subido ninguna imagen.'
+		});
+	}
+}
+
+function getImageFile(req, res) {
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/users/' + imageFile;
+
+	fs.exists(path_file, function(exists) {
+		if (exists) {
+			res.sendFile(path.resolve(path_file));
+		} else {
+			res.status(200).send({
+				message: 'No existe la imagen...'
+			});
+		}
+	});
+}
+
 module.exports = {
-	pruebas,
 	saveUser,
-	loginUser
+	loginUser,
+	updateUser,
+	uploadImage,
+	getImageFile
 };
--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,18 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated')
 
-api.get('/probado-controlador', UserController.pruebas);
-api.post('/register', UserController.saveUser);
+var multipart = require('connect-multiparty');
+var md_upload = multipart({
+	uploadDir: './uploads/users'
+})
+
+api.post('/register', md_auth.ensureAuth, UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/updateUser/:id', md_auth.ensureAuth, UserController.updateUser);
+api.post('/upload-image/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
+
 
 module.exports = api;
