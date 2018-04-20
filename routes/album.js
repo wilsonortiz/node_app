59e15a183971815ff13cb6d5c38892a2ed38c922
Wilson Ortiz Troncoso <w.ortizgeek@gmail.com>
2018-04-20 01:06:00 -0300

    CRUD get and save image album

--- routes/album.js
+++ routes/album.js
@@ -13,5 +13,10 @@ api.get('/album/:id', md_auth.ensureAuth, AlbumController.getAlbum);
 api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
 api.get('/albums/:artist?', md_auth.ensureAuth, AlbumController.getAlbums);
 api.put('/album/:id', md_auth.ensureAuth, AlbumController.updateAlbum);
+api.delete('/album/:id', md_auth.ensureAuth, AlbumController.deleteAlbum);
+api.post('/upload-image-album/:id', [
+	md_auth.ensureAuth, md_upload
+], AlbumController.uploadImage);
 
+api.get('/get-image-album/:imageFile', AlbumController.getImageFile);
 module.exports = api;
