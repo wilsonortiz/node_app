d346406cb6895b3242c10a83f5a4c42dcfa6ca55
Wilson Ortiz Troncoso <w.ortizgeek@gmail.com>
2018-04-21 00:25:46 -0300

    CRUD delete - uploadFile - getSongFile

--- controllers/song.js
+++ controllers/song.js
@@ -93,9 +93,84 @@ function updateSong(req, res) {
 
 }
 
+function deleteSong(req, res) {
+
+	var sondID = req.params.id;
+
+	Song.findByIdAndRemove(sondID, (err, songRemoved) => {
+		if (err) {
+			res.status(500).send({message: "Internal server error"});
+
+		} else if (!songRemoved) {
+			res.status(404).send({message: "Song not found"});
+
+		} else {
+			res.status(200).send({song: songRemoved});
+		}
+
+	});
+
+}
+
+function uploadFile(req, res) {
+	var songID = req.params.id;
+	var file_name = 'No subido...'
+
+	if (req.files) {
+		var file_path = req.files.file.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if (file_ext == 'mp3') {
+
+			Song.findByIdAndUpdate(songID, {
+				file: file_name
+			}, (err, songUpdated) => {
+
+				if (err) {
+					res.status(500).send({message: 'Error al actualizar la canción.'});
+
+				} else if (!songUpdated) {
+					res.status(404).send({message: 'Not found.'});
+
+				} else {
+					res.status(200).send({song: songUpdated});
+				}
+			});
+
+		} else {
+			res.status(200).send({message: 'Extensión del archivo no valida.'});
+		}
+
+		console.log(ext_split);
+
+	} else {
+		res.status(200).send({message: 'No existe el fichero de audio.'});
+	}
+}
+
+function getSongFile(req, res) {
+	var songFile = req.params.songFile;
+	var path_file = './uploads/songs/' + songFile;
+
+	fs.exists(path_file, function(exists) {
+		if (exists) {
+			res.sendFile(path.resolve(path_file));
+		} else {
+			res.status(200).send({message: 'No existe el fichero de audio.'});
+		}
+	});
+}
+
 module.exports = {
 	getSong,
 	saveSong,
 	getSongs,
-	updateSong
+	updateSong,
+	deleteSong,
+	uploadFile,
+	getSongFile
 }
